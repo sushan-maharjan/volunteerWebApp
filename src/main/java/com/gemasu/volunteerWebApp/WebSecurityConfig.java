614f93042061a1ff95d4643e8c188ef2a340a94f
alien <german.andres.segura@gmail.com>
2017-08-14 11:01:54 -0500

    Template working

--- src/main/java/com/gemasu/volunteerWebApp/WebSecurityConfig.java
+++ src/main/java/com/gemasu/volunteerWebApp/WebSecurityConfig.java
@@ -3,6 +3,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -16,8 +17,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 	UserDetailsService userDetailsService;
 	@Override
 	public void configure(HttpSecurity http) throws Exception{
+		
+		
 		http
 			.authorizeRequests()
+				.antMatchers("/webjars/**").permitAll()
+				.antMatchers("/static/**", "/", "/home", "/index", "/css/**", "/fonts/**", "/js/**", "/images/**", "**/favicon.ico").permitAll()
+				.antMatchers("/static/**", "/css/**", "/fonts/**", "/js/**", "/images/**", "**/favicon.ico").anonymous()
 				.antMatchers("/css/**", "/js/**", "/","/register","/home","/login","/createUser").permitAll()
 				.antMatchers("/addProject").hasAuthority("ADMIN")
 				.anyRequest().authenticated()
@@ -39,5 +45,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
 		auth.userDetailsService(userDetailsService);
 	}
+	
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+	    web
+	       .ignoring()
+	       .antMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/images/**");
+	}
+	
 
 }
