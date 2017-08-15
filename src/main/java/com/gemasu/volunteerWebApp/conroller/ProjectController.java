937d322d88674c18ba7438633cfee110ad57dc80
maggiedemeke <maggie>
2017-08-15 09:35:26 -0500

    Added a link to the projectList to direct to the detail's window
    added a method mapped to projectDetailsNew html that displays the
    details in projectController
    
    added a successful.html to display successful message, mapping method
    added to the controller

--- src/main/java/com/gemasu/volunteerWebApp/conroller/ProjectController.java
+++ src/main/java/com/gemasu/volunteerWebApp/conroller/ProjectController.java
@@ -1,6 +1,7 @@
 package com.gemasu.volunteerWebApp.conroller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,6 +28,23 @@ public class ProjectController {
 		model.addAttribute("title", "Update");
 		model.addAttribute("project",projectService.getOne(id));
 		return "project";
+	}
+	
+	
+	@RequestMapping("/{id}")
+	public String getaProject(Model model, @PathVariable int id){
+		/*System.out.println("ID: " + id);
+		model.addAttribute("action", "register");
+		model.addAttribute("title", "Apply");*/
+		model.addAttribute("project",projectService.getOne(id));
+		
+		
+		System.out.println("redirecting to projectDetail.html");
+		return "projectDetailnew";
+	}
+	@RequestMapping("/{id}/successful")
+	public String successful(Model model, @PathVariable int id){
+		return "SuccessfulApplication";
 	}
 }
 
